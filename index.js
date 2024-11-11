2cbcec9df8372c8dc9df72df81eb1cef0fa264f1
Raman Tyagi <Ramantyagi397@gmail.com>
2024-11-12 00:01:47 +0530

    done

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-let circularProcess = document.querySelector(".circular-bar"),
-processValue = document.querySelector(".process-bar");
+const circularProcesses = document.querySelectorAll(".circular-bar");
+ 
+const animateCircularBar = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const circularProcess = entry.target;
+      const processValue = circularProcess.querySelector(".process-bar");
+      let processStartValue = 0;
+      const processEndValue = parseInt(processValue.textContent);
+      const speed = 25;
+ 
+      const process = setInterval(() => {
+        processStartValue++;
+        processValue.textContent = `${processStartValue}%`;
+        circularProcess.style.background = `conic-gradient(#7324f3 ${processStartValue * 3.6}deg, #ededed 0deg)`;
 
-
-let processStartValue = 0,
-processEndValue = 90,
-speed = 25;
-
-let process = setInterval(() => {
-    processStartValue++;
-    processValue.textContent = `${processStartValue}%`
-    circularProcess.style.background = `conic-gradient(#7324f3 ${processStartValue * 3.6}deg, #ededed 0deg)`
-
-
-    if(processStartValue == processEndValue){
-        clearInterval(process);
+        if (processStartValue === processEndValue) {
+          clearInterval(process);
+          observer.unobserve(circularProcess); 
+        }
+      }, speed);
     }
-},speed);
+  });
+};
+ 
+const options = {
+  root: null, 
+    threshold: 0.3,
+};
+
+const observer = new IntersectionObserver(animateCircularBar, options);
+
+
+circularProcesses.forEach((circularProcess) => {
+  observer.observe(circularProcess);
+});
